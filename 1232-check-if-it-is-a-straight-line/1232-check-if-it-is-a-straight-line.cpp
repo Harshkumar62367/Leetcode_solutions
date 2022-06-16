9d58511f732fd4f462ad5e848449fcfe01e58056
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-06-16 23:22:41 +0530

    Time: 24 ms (7.68%), Space: 10.1 MB (78.48%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -3,16 +3,16 @@ public:
     bool checkStraightLine(vector<vector<int>>& c) {
         for(int i=0;i<c.size()-2;i++)
         {
-            cout<<i<<endl;
+            //cout<<i<<endl;
             if((c[i+2][1]-c[i+1][1])*(c[i+1][0]-c[i][0])!= (c[i+1][1]-c[i][1])*(c[i+2][0]-c[i+1][0]))
             {
                 // cout<<(c[i+2][1]-c[i+1][1])<<"\n";
                 // cout<<(c[i+1][0]-c[i][0])<<"\n";
                 // cout<<(c[i+1][1]-c[i][1])<<"\n";
                 // cout<<(c[i+2][0]-c[i+1][0])<<"\n";
-                cout<<i<<endl;
-                cout<<(c[i+2][1]-c[i+1][1])*(c[i+1][0]-c[i][0])<<endl;
-                cout<<(c[i+1][1]-c[i][1])*(c[i+2][0]-c[i+1][0])<<endl;
+                // cout<<i<<endl;
+                // cout<<(c[i+2][1]-c[i+1][1])*(c[i+1][0]-c[i][0])<<endl;
+                // cout<<(c[i+1][1]-c[i][1])*(c[i+2][0]-c[i+1][0])<<endl;
                 return false;
             }
         }
