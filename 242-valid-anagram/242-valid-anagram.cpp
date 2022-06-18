18202bfb9b40feab0035ff0d95006d25e96d8c7f
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-06-18 23:05:06 +0530

    Time: 8 ms (80.65%), Space: 7.4 MB (45.73%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         int a[123]={0},b[123]={0};
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
         for(int i=0;i<s.size();i++)
         {
             int x=s[i];
-            a[x]++;
-        }
-        for(int i=0;i<t.size();i++)
-        {
             int y=t[i];
+            a[x]++;
             b[y]++;
         }
         for(int i=96;i<123;i++)
