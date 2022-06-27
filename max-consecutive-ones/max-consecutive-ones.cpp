ffe0992090a5b34cab9124eaccd61fe4879237fe
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-06-27 16:27:52 +0530

    Time: 79 ms, Memory: 36.2 MB - LeetHub

--- max-consecutive-ones/max-consecutive-ones.cpp
+++ max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,18 +4,18 @@ public:
         int maxm=0,count=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==0)
+            if(nums[i]==1)
             {
-                count = 0;
-            }
-            else{
                 count++;
                 if(count>maxm)
                 {
                     maxm=count;
                 }
             }
+            else{
+                count=0;
+            }
         }
-        return maxm; 
+        return maxm;
     }
 };
