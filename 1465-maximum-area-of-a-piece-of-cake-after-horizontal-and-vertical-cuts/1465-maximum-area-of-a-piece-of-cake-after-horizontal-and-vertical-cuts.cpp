a28dee99e2ffa0be210c509b85ee303830bac9a6
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-07-02 23:35:47 +0530

    Time: 140 ms (14.08%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -3,12 +3,12 @@ public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
-        long long int m=horizontalCuts[0];
-        long long int n=verticalCuts[0];
-        long long int x = h-horizontalCuts[horizontalCuts.size()-1];
-        long long int y = w-verticalCuts[verticalCuts.size()-1];
-        long long int a=max(m,x);
-        long long int b=max(n,y);
+         long long int m=horizontalCuts[0];
+         long long int n=verticalCuts[0];
+         long long int x = h-horizontalCuts[horizontalCuts.size()-1];
+         long long int y = w-verticalCuts[verticalCuts.size()-1];
+         long long int a=max(m,x);
+         long long int b=max(n,y);
         for(int i=0;i<horizontalCuts.size()-1;i++)
         {
             if(a<horizontalCuts[i+1]-horizontalCuts[i])
