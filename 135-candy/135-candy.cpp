b725faf744bceb065b428844d0eee11a545ee43a
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-07-05 01:33:10 +0530

    Time: 34 ms (49.67%), Space: 17.8 MB (41.00%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,1);
+    
+    for(int i=1;i<n;i++)
+    {
+        if(nums[i]>nums[i-1] )
+        {
+            ans[i]=ans[i-1]+1;
+            
+        }
+    }
+    for(int i=n-2;i>=0;i--)
+    {
+        if(nums[i]>nums[i+1] and ans[i]<=ans[i+1])
+        {
+            ans[i]=ans[i+1]+1;
+        }
+    }
+    int sum=0;
+    for(auto itr:ans)
+    {
+        sum+=itr;
+    }
+    
+    return sum;
+    }
+};
