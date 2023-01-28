a2df60877678dec0ecfe76bfaf6dc7a0d3b9cd66
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2023-01-28 11:13:46 +0530

    Time: 31 ms (27.43%), Space: 15.7 MB (83.72%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int count=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<=nums[i-1])
+            {
+                count+=nums[i-1]-nums[i]+1;
+                nums[i]=nums[i-1]+1;
+            }
+            
+        }
+        return count;
+    }
+};
