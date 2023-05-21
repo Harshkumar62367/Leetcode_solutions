620b4ccb76528c00ae07586891bbb024552aeac5
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2023-05-21 15:39:55 +0530

    Time: 12 ms (7.51%), Space: 9.9 MB (12.79%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0;
         for(int i=0;i<nums.size();i++)
         {
-            string s1=to_string(nums[i]);
-            if(s1.size()%2==0)
+            int x = floor(log10(nums[i])+1);
+            if(x%2==0)
             {
                 count++;
             }
