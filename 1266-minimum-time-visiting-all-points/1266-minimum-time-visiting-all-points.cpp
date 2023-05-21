341bd7cef51d4825c9c16a0d010f29bff60d180f
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2023-05-21 14:45:17 +0530

    Time: 5 ms (86.62%), Space: 10 MB (92.00%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int count=0;
-        for(int i=0;i<points.size()-1;i++)
+        for(int i=1;i<points.size();i++)
         {
-            count+=max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1]));
+            count+=max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
         }
         return count;
-        
     }
 };
+
+// easy
+//  not revise
