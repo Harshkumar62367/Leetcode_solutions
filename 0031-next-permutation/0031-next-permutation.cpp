7f5b1176afe4127540ef89b83980c87315512f57
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2024-03-31 11:39:07 +0530

    Time: 4 ms (58.20%), Space: 14.6 MB (19.66%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int ind = -1;
+        int n=nums.size();
+        for(int i=n-1;i>=1;i--){
+            if(nums[i]>nums[i-1]){
+                ind = i-1;
+                break;
+            }
+        }
+        cout<<ind;
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
     }
 };
