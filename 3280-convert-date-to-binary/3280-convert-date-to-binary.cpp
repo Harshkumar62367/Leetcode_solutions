0b5a7ec54920f540954c94c1ef88752f525b2b96
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2024-12-17 21:17:46 +0530

    Time: 2 ms (38.35%), Space: 9.4 MB (54.81%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
@@ -1,47 +1,34 @@
 class Solution {
+    
+    string convert(int num){
+        //cout<<num<<"\n";
+            string s;
+            while(num){
+                s+=to_string(num%2);
+                num>>=1;
+            }
+            reverse(s.begin(), s.end());
+        //cout<<s<<"\n";
+            return s;
+        }
+    
 public:
     string convertDateToBinary(string date) {
-        string year1 = date.substr(0, 4);
-        int year=stoi(year1);
-        string month1 = date.substr(5, 2);
-        int month=stoi(month1);
-        string day1 = date.substr(8, 2);
-        int day=stoi(day1);
-        //cout<<year<<" "<<month<<" "<<day<<"\n";
         string res="";
-        string res_year="",res_month="",res_day=""; 
-        while(year){
-            if(year&1){
-                res_year+='1';
+        string temp="";
+        for(auto it: date){
+            if(it=='-'){
+                //cout<<temp<<"\n";
+                if(!res.empty()) res+='-';
+                res+=convert(stoi(temp));
+                temp="";
             }else{
-                res_year+='0';
+                temp+=it;
             }
-            year>>=1;
+            //cout<<it<< "  ";
         }
-        reverse(res_year.begin(), res_year.end());
-        res+=res_year;
         res+='-';
-        while(month){
-            if(month&1){
-                res_month+='1';
-            }else{
-                res_month+='0';
-            }
-            month>>=1;
-        }
-        reverse(res_month.begin(), res_month.end());
-        res+=res_month;
-        res+='-';
-        while(day){
-            if(day&1){
-                res_day+='1';
-            }else{
-                res_day+='0';
-            }
-            day>>=1;
-        }
-        reverse(res_day.begin(), res_day.end());
-        res+=res_day;
+        res+=convert(stoi(temp));
         return res;
         
     }
