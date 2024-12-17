f416b0ab923eae112a85aad16d6bf3b5cddaeb09
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2024-12-17 21:54:35 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (26.43%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,17 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n){
-            n=n&(n-1);
-            count++;
+            count+=(n&1);
+            n>>=1;
         }
         return count;
     }
 };
+
+
+// int count = 0;     Method - 1
+//     while(n){
+//         n=n&(n-1);
+//         count++;
+//     }
+//     return count;
