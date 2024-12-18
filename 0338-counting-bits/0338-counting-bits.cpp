b3f1018ed7b2f5e98a6f4ca6ede3007a2944e4f1
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2024-12-18 19:51:39 +0530

    Time: 3 ms (24.70%), Space: 10.6 MB (26.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,6 +4,9 @@ public:
         vector<int> vect(n+1);
         for(int i=1;i<=n;i++){
             vect[i]=vect[i>>1] + (1&i);
+            
+            // For Odd: Odd number has same number of bits than its half value + 1;
+            // For Even: Even number has same number of bits than its half value + 0;
         }
         return vect;
     }
