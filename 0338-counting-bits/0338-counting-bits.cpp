17effad64a7fce7703f8d26bed86a5eefd823c4f
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2024-12-18 19:50:18 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (26.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> vect;
-        for(int i=0;i<=n;i++){
-            int num = i, count=0;
-            while(num){
-                num=num&(num-1);
-                count++;
-            }
-            vect.push_back(count);
+        vector<int> vect(n+1);
+        for(int i=1;i<=n;i++){
+            vect[i]=vect[i>>1] + (1&i);
         }
         return vect;
     }
 };
+
+
+//vector<int> vect;
+    // for(int i=0;i<=n;i++){
+    //     int num = i, count=0;
+    //     while(num){
+    //         num=num&(num-1);
+    //         count++;
+    //     }
+    //     vect.push_back(count);
+    // }
+    // return vect;
